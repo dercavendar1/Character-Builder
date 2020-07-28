ef0c3294e7d29672216928c8f850b2927e168832
Brian Cavender <dercavendar@gmail.com>
2020-07-28 11:31:28 -0500

    added enemies.py with empty classes for some enemy types

---
+++ enemies.py
@@ -0,0 +1,17 @@
+import math
+import random
+
+import funcs
+from character import Character
+
+
+class Dragon(Character):
+    pass
+
+
+class Abeloth(Character):
+    pass
+
+
+class Kraken(Character):
+    pass
