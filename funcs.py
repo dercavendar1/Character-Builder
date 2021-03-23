3155677864d6ca27652cf3b17ad13d2ea2a54b12
Brian Cavender <brian.cavender3@gmail.com>
2021-03-22 20:53:56 -0500

    Minor refactoring for style compliance

--- funcs.py
+++ funcs.py
@@ -10,32 +10,25 @@ from rogue import *
 from wizard import *
 
 # DICE ROLL FUNCTIONS#################################
-
-
 def d20(min_roll):
     roll = random.randint(min_roll, 20)
     return roll
-
 
 def d16(min_roll):
     roll = random.randint(min_roll, 16)
     return roll
 
-
 def d12(min_roll):
     roll = random.randint(min_roll, 12)
     return roll
-
 
 def d8(min_roll):  # method for rolling an 8 sided die
     roll = random.randint(min_roll, 8)
     return roll
 
-
 def d6(min_roll):  # method for rolling an 6 sided die
     roll = random.randint(min_roll, 6)
     return roll
-
 
 def d4(min_roll):  # method for rolling an 4 sided die
     roll = random.randint(min_roll, 4)
@@ -47,7 +40,7 @@ def d4(min_roll):  # method for rolling an 4 sided die
 
 
 def battle(player, enemy):
-    # Battle Loop for both Barbarian Classes########################################
+# Battle Loop for both Barbarian Classes########################################
     if(isinstance(player, Barbarian)):
         barbarian_fight(player, enemy)
 # Battle Loop for Bard##########################################################
